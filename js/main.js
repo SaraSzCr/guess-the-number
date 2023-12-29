87e7282a160677c17709a11fcc32c135a4a8c256
Sara Sanchez Crespo <saritasc89@gmail.com>
2023-12-29 13:31:46 +0100

    Add parseInt and range (0-100)

--- js/main.js
+++ js/main.js
@@ -5,47 +5,43 @@
 const mainButton = document.querySelector(".js_button");
 const userInput = document.querySelector(".js_inputNumber");
 const paragraphResult = document.querySelector(".js_paragraphResult");
-const userAttempts = document.querySelector('.js_attempts');
+const userAttempts = document.querySelector(".js_attempts");
 
-
-//FUNCIONES
+//VARIABLES
 
 let attempts = 0;
 
-paragraphResult.innerHTML = "Pista: Escribe el número y dale a prueba";
+paragraphResult.innerHTML = " Escribe el número y dale a prueba";
+
+//FUNCIONES
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 const randomNumber = getRandomNumber(100);
 
-//parseInt?
-
 
 const textResult = () => {
-  const inputValue = userInput.value;
-
+  const inputValue = parseInt(userInput.value);
   console.log(inputValue, randomNumber);
 
   if (inputValue > randomNumber) {
     paragraphResult.innerHTML = "Demasiado alto";
   } else if (inputValue < randomNumber) {
     paragraphResult.innerHTML = "Demasiado bajo";
-  } else if (inputValue >= randomNumber) {
+  } else if (inputValue === randomNumber) {
     paragraphResult.innerHTML = "Has ganado campeona!!!";
-   } else if (inputValue === randomNumber)  {
+  } else if (inputValue < 1 || inputValue > 100) {
     paragraphResult.innerHTML = "El número debe estar entre 1 y 100";
   } else {
     paragraphResult.innerHTML = "El número debe estar entre 1 y 100";
   }
-  
 };
 
 const numberOfAttempts = () => {
-attempts ++;
-userAttempts.innerHTML = attempts;
+  attempts++;
+  userAttempts.innerHTML = attempts;
 };
-
 
 //FUNCIONES/EVENTOS
 
