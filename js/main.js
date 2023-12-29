8d6efd0a14b0d54a44ca1cbf6891bf1af32c413e
Sara Sanchez Crespo <saritasc89@gmail.com>
2023-12-29 14:15:58 +0100

    Update comparisons and  add functions

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ const userAttempts = document.querySelector(".js_attempts");
 
 //VARIABLES
 
+const randomNumber = getRandomNumber(100);
+
 let attempts = 0;
 
 paragraphResult.innerHTML = " Escribe el número y dale a prueba";
@@ -18,35 +20,51 @@ paragraphResult.innerHTML = " Escribe el número y dale a prueba";
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-const randomNumber = getRandomNumber(100);
 
+
+const feedbackClue = (message) => {
+  paragraphResult.innerHTML = message;
+}
 
 const textResult = () => {
+
   const inputValue = parseInt(userInput.value);
   console.log(inputValue, randomNumber);
 
-  if (inputValue > randomNumber) {
-    paragraphResult.innerHTML = "Demasiado alto";
-  } else if (inputValue < randomNumber) {
-    paragraphResult.innerHTML = "Demasiado bajo";
-  } else if (inputValue === randomNumber) {
-    paragraphResult.innerHTML = "Has ganado campeona!!!";
-  } else if (inputValue < 1 || inputValue > 100) {
-    paragraphResult.innerHTML = "El número debe estar entre 1 y 100";
-  } else {
-    paragraphResult.innerHTML = "El número debe estar entre 1 y 100";
+  if (isNaN (inputValue)){
+    feedbackClue ('El número debe estar entre 1 y 100.');
   }
+  else if (inputValue < 1 || inputValue > 100) {
+    feedbackClue ('El número debe estar entre 1 y 100.');
+  }
+  else if (inputValue === randomNumber) {
+    feedbackClue ('Has ganado campeona!!!');
+  }
+  else if (inputValue > randomNumber) {
+    feedbackClue(' Demasiado alto.');
+  } 
+  else if (inputValue < randomNumber) {
+    feedbackClue('Demasiado bajo.');
+  }  
+  
+  
+};
+
+const writeAttempts = () => {
+  userAttempts.innerHTML = attempts;
 };
 
 const numberOfAttempts = () => {
   attempts++;
-  userAttempts.innerHTML = attempts;
+  writeAttempts();
 };
 
 //FUNCIONES/EVENTOS
 
 const handleClickButton = (event) => {
   event.preventDefault();
+
+  
 
   textResult();
 
