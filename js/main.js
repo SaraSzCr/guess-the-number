bb372d78cc4e305936146eccf074c1ed64333108
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-01-04 18:49:02 +0100

    Generate new randomNumber

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const resetBtn = document.querySelector(".js_resetBtn");
 
 //VARIABLES
 
-const randomNumber = getRandomNumber(100); //9
+let randomNumber = getRandomNumber(100);
 
 let attempts = 0;
 
@@ -29,15 +29,15 @@ const showResultMessage = () => {
   console.log(inputValue, randomNumber);
 
   if (isNaN(inputValue)) {
-    paintClue('El número debe estar entre 1 y 100.');
+    paintClue("El número debe estar entre 1 y 100.");
   } else if (inputValue < 1 || inputValue > 100) {
-    paintClue('El número debe estar entre 1 y 100.');
+    paintClue("El número debe estar entre 1 y 100.");
   } else if (inputValue === randomNumber) {
-    paintClue('Has ganado campeona!!!');
+    paintClue("Has ganado campeona!!!");
   } else if (inputValue > randomNumber) {
-    paintClue('Demasiado alto.');
+    paintClue("Demasiado alto.");
   } else if (inputValue < randomNumber) {
-    paintClue('Demasiado bajo.');
+    paintClue("Demasiado bajo.");
   }
   if (attempts >= 5) {
     paintClue(' "The game is over". ');
@@ -71,7 +71,7 @@ const handleClickButton = (event) => {
 mainButton.addEventListener("click", handleClickButton);
 
 const handleClickResetBtn = (event) => {
-  const randomNumber = getRandomNumber(100);
+  randomNumber = getRandomNumber(100);
 
   resetAttempts();
   paintClue("Escribe el número y dale a prueba");
